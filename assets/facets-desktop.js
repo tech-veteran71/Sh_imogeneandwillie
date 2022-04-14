a28649e107cb2a8a497cd1d7e8afc6c4bd3ad47a
tech-veteran71 <tech.veteran71@gmail.com>
2022-04-14 11:17:50 -0700

    implemented mobile filters and adapted desktop logic

--- assets/facets-desktop.js
+++ assets/facets-desktop.js
@@ -3,11 +3,11 @@ class FacetsDesktop extends HTMLElement {
     super();
 
     this._selectors = {
-      form: 'form',
-      accordionOpen: '[js-accordion-open]',
-      accordionContent: '[js-accordion-content]',
-      accordionClose: '[js-accordion-close]'
-    }
+      form: "form",
+      accordionOpen: "[js-accordion-open]",
+      accordionContent: "[js-accordion-content]",
+      accordionClose: "[js-accordion-close]",
+    };
   }
 
   connectedCallback() {
@@ -15,51 +15,47 @@ class FacetsDesktop extends HTMLElement {
     this._initForm();
   }
 
-  _initAccordion() { 
-    this.querySelectorAll(this._selectors.accordionOpen).forEach(button => {
-      button.addEventListener('click', toggleContent.bind(this))
-    })
+  _initAccordion() {
+    this.querySelectorAll(this._selectors.accordionOpen).forEach((button) => {
+      button.addEventListener("click", toggleContent.bind(this));
+    });
 
-    function toggleContent(e)  {
+    function toggleContent(e) {
       e.stopImmediatePropagation();
       const trigger = e.currentTarget;
-      const content = this.querySelector(`#${trigger.getAttribute('aria-controls')}`);
-      let isExpanded = trigger.getAttribute('aria-expanded');
-      if (isExpanded == 'false') {
-        trigger.setAttribute('aria-expanded', true);
-        content.setAttribute('aria-hidden', true);
-      }else{
-        trigger.setAttribute('aria-expanded', false);
-        content.setAttribute('aria-hidden', false);
-  
+      const content = this.querySelector(
+        `#${trigger.getAttribute("aria-controls")}`
+      );
+      let isExpanded = trigger.getAttribute("aria-expanded");
+      if (isExpanded == "false") {
+        trigger.setAttribute("aria-expanded", true);
+        content.setAttribute("aria-hidden", false);
+      } else {
+        trigger.setAttribute("aria-expanded", false);
+        content.setAttribute("aria-hidden", true);
       }
     }
   }
 
   _initForm() {
+    const facets = document.querySelector('facet-filters-form');
     const masterForm = document.getElementById("FacetFiltersFormMobile");
     const form = this.querySelector(this._selectors.form);
 
-    form.addEventListener('input', (e) => {
+    form.addEventListener("input", (e) => {
       const input = e.target;
-      if (input.name === 'sort_by') {
-        const msInput = masterForm.querySelector(`[name="sort_by"]`);
-        if (msInput) {
-          msInput.value = input.value;
-        }
-      } else {
-        const msInput = masterForm.querySelector(`[name="${input.name}"][value="${input.value}"]`);
-        if (msInput) {          
-          msInput.checked = input.checked;
-        }
-      }
+      const msInput = masterForm.querySelector(
+        `[name="${input.name}"][value="${input.value}"]`
+      );
+      if (!msInput) return;
 
-      masterForm.dispatchEvent(new Event('input'))
-
-
-    })
+      /** Mechanism to avoid toggling facets on desktop */
+      FacetFiltersForm.toggleFacets = false;
+      
+      msInput.checked = input.checked;
+      masterForm.dispatchEvent(new Event("input"));
+    });
   }
-
 }
 
-customElements.define('facets-desktop', FacetsDesktop);
+customElements.define("facets-desktop", FacetsDesktop);
