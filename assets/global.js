b9773f7fc85a4830395762df2094eea5717ed64b
tech-veteran71 <tech.veteran71@gmail.com>
2022-05-24 13:26:38 -0700

    QA updates

--- assets/global.js
+++ assets/global.js
@@ -3151,6 +3151,20 @@ customElements.define("ajax-cart", AjaxCart);
     }
   });
 
+  new Swiper(".slide-section .product-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  })
+
   // Denim collection swiper script
   new Swiper('.denim-collection .product-swiper', {
     slidesPerView: 4,
@@ -3340,3 +3354,31 @@ function addSingle() {
     el.classList.remove("active");
   })
 }
+
+(
+  function() {
+    function updSwiperNumericPagination() {
+      this.el.querySelector(".swiper-counter").innerHTML = '<span class="count">' + (this.realIndex + 1) + '</span>/<span class="total">' + this.el.slidesQuantity + "</span>";
+    }
+    const slider = document.querySelectorAll('.product-slider-section .product-slider');
+    slider.forEach( el => {
+      // Getting slides quantity before slider clones them
+      el.slidesQuantity = el.querySelectorAll(".swiper-slide").length;
+
+      // Swiper initialization
+      new Swiper(el, {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: el.querySelector(".swiper-button-next"),
+          prevEl: el.querySelector(".swiper-button-prev")
+        },
+        on: {
+          init: updSwiperNumericPagination,
+          slideChange: updSwiperNumericPagination
+        }
+      });
+    })
+  }
+)()
